60cf8863c45ff17b829043b20ee1e8e65124a9e1
wyne <wyneshalitie@gmail.com>
2023-05-31 13:58:27 +0300

    test

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>CMPE 212 DEMO</h1> <h4>Message: Success.</h4> <p>Version 1.1.1</p>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
 
-app.get('/students', (req, res) => {
+app.get('/products', (req, res) => {
   res.send([
     {
-      StuName: 'Asli',
-      StuId: 121200107
+      productId: '101',
+      price: 100
     },
     {
-      StuName: 'Tugce',
-      StuId: 122130183
+      productId: '102',
+      price: 150
     }
   ])
 })
@@ -23,5 +23,3 @@ app.listen(port, ()=> {
   console.log(`Demo app is up and listening to port: ${port}`);
 })
  
-
-
